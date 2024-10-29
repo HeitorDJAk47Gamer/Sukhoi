732794c3bdca33ee67f40c237e42dae96b0304a7
Heitor Machado <73564945+HeitorDJAk47Gamer@users.noreply.github.com>
2024-10-29 09:47:21 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import random
+from flaskapp import run
+
+sukhoi = commands.Bot(command_prefix = '!',case_insensitive = True, intents=discord.Intents.all())
+@sukhoi.event
+async def on_ready():
+	Y = sukhoi.latency * 1000
+	A = round(Y)
+	print(f'Ping atual: {A}')
+	print(f'Olha esses canardes de fora {sukhoi.user.id}')
+
+
+@sukhoi.command()
+async def d(ctx, y=0, numero=0, h="nada", mod=0):
+  x = []
+  while 0 < y:
+      x.append(random.randint(1, numero))
+      y = (y - 1)
+  total = sum(x)
+  if numero != 0 and h == "nada":
+      await ctx.reply(f"`{total}` <- {x}")
+  elif numero != 0 and h == "+":
+    ad = (total + mod)
+    await ctx.reply(f"`{ad}` <- {x} + {mod}")
+  elif numero != 0 and h == "-":
+    men = (total - mod)
+    await ctx.reply(f"`{men}` <- {x} - {mod}")
+  else:
+    await ctx.reply ("*Defina um valor válido*")
+	
+@sukhoi.command()
+async def ping(ctx):
+  x = sukhoi.latency * 1000
+  y = round(x)
+  await ctx.send(f"{y} ms")
+
+sukhoi.run('token')
